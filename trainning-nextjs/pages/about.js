7dbed75c9c1996d24d5d0f7e5da9656578e2c8a6
rinnv <rinnv@vnext.com.vn>
2020-09-10 13:24:35 +0700

    editt npm start

---
+++ trainning-nextjs/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+about.propTypes = {
+    
+};
+
+function about(props) {
+    return (
+        <div>
+            <h4>Trang about</h4>
+        </div>
+    );
+}
+
+export default about;
