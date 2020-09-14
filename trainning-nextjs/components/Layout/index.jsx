0278f329d0a1157523bcd9fd420c1a1431521ce2
rinnv <rinnv@vnext.com.vn>
2020-09-14 09:27:39 +0700

    import images

---
+++ trainning-nextjs/components/Layout/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Layout.propTypes = {
+    
+};
+
+function Layout(props) {
+    return (
+        <div>
+            <h5>trang lay out</h5>
+        </div>
+    );
+}
+
+export default Layout;
--- trainning-nextjs/pages/index.tsx
+++ trainning-nextjs/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-
+import Image from '../assets/abc.jpg'
+import Layout from '../components/Layout'
 function Home() {
   return (
     <div className={styles.container}>
@@ -9,12 +10,15 @@ function Home() {
         <title>Create Next App edit</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <img src={Image} />
+      <img src="/images/image003_vbgj.jpg" />
       <Link href="/login">
           <a>Login</a>
       </Link>
       <Link href="/user/10">
           <a>User</a>
       </Link>
+      <Layout/>
     </div>
   )
 }
